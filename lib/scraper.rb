c187e7e252b4069334d9fe1c912894c492e6ead4
Candice Peters <candicelizabethpeters@gmail.com>
2019-09-11 16:03:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,7 +3,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-   page = Nokogiri::HTML(open(index_url)) 
+   page = Nokogiri::HTML(open(./fixtures/student-site/index.html)) 
   end
 
   def self.scrape_profile_page(profile_url)
